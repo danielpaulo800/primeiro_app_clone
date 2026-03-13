12729bbda95c5acb0bdc1ae8d3130cc0f8a49cac
danielpaulo800 <00001117545891sp@al.educacao.sp.gov.br>
2026-03-13 14:28:33 -0300

    Adicionando projeto

---
+++ Anhanguera_primeiroapp/Controllers/AlunoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Anhanguera_primeiroapp.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
